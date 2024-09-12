2873a8eb8877a4849df35a09ef4c606a99ea1ce4
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-09-11 21:46:48 -0400

    Part 3 Revised

--- script.js
+++ script.js
@@ -33,14 +33,11 @@ for (let i = 1; i <= 100; i++){
 
 // Part 3: Feeling Loopy ---------------------------------------------------------------------------
 // Loop through the characters of a given CSV string.
-let str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`
-for(let char of str){
-    console.log(char)
-    if(char == `\n`)
-        console.log(`Space`)
-
-
+    // for(let i of str){
+    //     console.log(i)
+    // }
 // Store each “cell” of data in a variable.
+let str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`
 let cell1 = `ID`
 let cell2 = `Name`
 let cell3 = `Occupation`
@@ -63,18 +60,11 @@ let cell19 = `Doctor's Assistant`
 let cell20 = `26`
 
 // When you encounter a comma, move to the next cell.
-
+// When you encounter the “\r\n” sequence, move to the next “row.”
 for (let i of str){
     console.log(i);
     if (i == ",")
         console.log(`Move to the next cell`, i)
-}
-
-// When you encounter the “\r\n” sequence, move to the next “row.”
-
-
-for (let i of str){
-    console.log(i);
     if (i == "\n")
         console.log(`Move to the next row`, i)
 }
@@ -92,3 +82,13 @@ console.log(cell17, cell18, cell19, cell20);
 // You can make the following assumptions:
 //     There will only be 4 cells per row.
 //     There will be no escaped characters other than “\n."
+
+// Use the example string provided above to test your program. Once you are confident it is working correctly, try the following string to see if your program works properly with other data.
+let char = `Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232`
+for (let i of char){
+    console.log(i);
+    if (i == ",")
+        console.log(`Move to the next cell`, i)
+    if (i == "\n")
+        console.log(`Move to the next row`, i)
+}
